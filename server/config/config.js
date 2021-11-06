72cffc66ccab29b3d77bdd547f891362f9be7be6
krakerbrain <marioplantabaja@gmail.com>
2021-11-05 22:57:00 -0300

    Variable de entorno Mongo

--- server/config/config.js
+++ server/config/config.js
@@ -18,7 +18,7 @@ let urlDB;
 if (process.env.NODE_ENV === "dev") {
   urlDB = "mongodb://localhost:27017/cafe";
 } else {
-  urlDB = "mongodb+srv://krakerbrain:6PoLkO9ORPh8d0eM@cluster0.7k7ui.mongodb.net/cafe";
+  urlDB = process.env.MONGO_URL;
 }
 
 process.env.URLDB = urlDB;
